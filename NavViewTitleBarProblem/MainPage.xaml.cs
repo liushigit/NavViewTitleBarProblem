3578420f8e643b80421ca0f5f40065ff300e6e2b
Liu Shi <liushie@gmail.com>
2018-06-29 18:52:46 +0800

    added the demo

--- NavViewTitleBarProblem/MainPage.xaml.cs
+++ NavViewTitleBarProblem/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,39 @@ namespace NavViewTitleBarProblem
         public MainPage()
         {
             this.InitializeComponent();
+
+            //draw into the title bar
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+
+
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            Window.Current.SetTitleBar(AppTitle); // Custom title bar
+
+            //remove the solid-colored backgrounds behind the caption controls and system back button
+            var viewTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            viewTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            viewTitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            viewTitleBar.ButtonForegroundColor = (Color)Resources["SystemBaseHighColor"];
+
+            void UpdateAppTitle()
+            {
+                var full = (ApplicationView.GetForCurrentView().IsFullScreenMode);
+                var left = 12 + (full ? 0 : CoreApplication.GetCurrentView().TitleBar.SystemOverlayLeftInset);
+                AppTitle.Margin = new Thickness(left, 8, 0, 0);
+            }
+
+
+            Window.Current.CoreWindow.SizeChanged += (s, e) => UpdateAppTitle();
+            coreTitleBar.LayoutMetricsChanged += (s, e) => UpdateAppTitle();
+        }
+
+        // https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/navigationview#extending-your-app-into-the-title-bar
+        
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            navView.IsBackEnabled = true;
         }
     }
 }
